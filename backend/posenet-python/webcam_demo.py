020f2a9f6ffb2f9e5c65062a8e6e94e4d5f780e8
minnseong <kdsvip5@naver.com>
2021-08-11 17:37:51 +0900

    MODIFY webcam_demo.py - Spine At The Shoulder, Middle Of The Spine, Base Of Spine 키포인트 추가

--- backend/posenet-python/webcam_demo.py
+++ backend/posenet-python/webcam_demo.py
@@ -1,11 +1,10 @@
+import posenet
 import tensorflow as tf
 import cv2
 import time
 import argparse
-
-import posenet
-
-tf = tf.compat.v1
+import numpy as np
+np.set_printoptions(threshold=np.inf, linewidth=np.inf)
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--model', type=int, default=101)
@@ -16,6 +15,22 @@ parser.add_argument('--scale_factor', type=float, default=0.7125)
 parser.add_argument('--file', type=str, default=None,
                     help="Optionally use a video file instead of a live camera")
 args = parser.parse_args()
+
+position = ["코", "왼쪽눈", "오른쪽눈", "왼쪽귀", "오른쪽귀", "왼쪽어깨", "오른쪽어깨", "왼쪽팔꿈치", "오른쪽팔꿈치",
+            "왼쪽손목", "오른쪽손목", "왼쪽골반부위", "오른쪽골반부위", "왼쪽무릎", "오른쪽무릎", "왼쪽발목", "오른쪽발목"]
+
+# 철추상 : Spine At The Shoulder , 척추중 : Middle Of The Spine , 척추하 : Base Of Spine
+spine_position = ["척추상", "척추중", "척추하"]
+
+# spine position을 구하기 위해 평균 구하는 함수.
+def getAverage(pos, n):
+    x, y = 0, 0
+
+    for i in range(n):
+        x += pos[i][0]
+        y += pos[i][1]
+
+    return [x/n, y/n]
 
 
 def main():
@@ -32,6 +47,7 @@ def main():
 
         start = time.time()
         frame_count = 0
+        iii = 0
         while True:
             input_image, display_image, output_scale = posenet.read_cap(
                 cap, scale_factor=args.scale_factor, output_stride=output_stride)
@@ -50,13 +66,38 @@ def main():
                 max_pose_detections=10,
                 min_pose_score=0.15)
 
+            # 0:코, 1:왼쪽눈, 2:오른쪽눈, 3:왼쪽귀, 4:오른쪽귀", 5:왼쪽어깨, 6:오른쪽어깨
+            # 7:왼쪽팔꿈치, 8:오른쪽팔꿈치, 9:왼쪽손목, 10:오른쪽손목, 11:왼쪽골반부위, 12:오른쪽골반부위
+            # 13:왼쪽무릎, 14:오른쪽무릎, 15:왼쪽발목, 16:오른쪽발목
+            # 17:척추상 = 5/6평균, 18:척추중 = 5/6/11/12평균 19:척추하 = 11/12평균
+
+            spineTop = getAverage(
+                [keypoint_coords[0][5], keypoint_coords[0][6]], 2)
+            spineMiddle = getAverage(
+                [keypoint_coords[0][5], keypoint_coords[0][6], keypoint_coords[0][11], keypoint_coords[0][12]], 4)
+            spineBottom = getAverage(
+                [keypoint_coords[0][11], keypoint_coords[0][12]], 2)
+
+            spine_pos = [spineTop, spineMiddle, spineBottom]
+
+            for i in range(3):
+                tmp = np.array([[spine_pos[i]], [[0.0, 0.0]], [[0.0, 0.0]], [[0.0, 0.0]], [[0.0, 0.0]], [[
+                    0.0, 0.0]], [[0.0, 0.0]], [[0.0, 0.0]], [[0.0, 0.0]], [[0.0, 0.0]]])
+                keypoint_coords = np.concatenate(
+                    (keypoint_coords, tmp), axis=1)
+                keypoint_scores = np.concatenate(
+                    (keypoint_scores, np.array([[1], [0.00000000e+00], [0.00000000e+00], [0.00000000e+00], [0.00000000e+00], [0.00000000e+00], [0.00000000e+00], [0.00000000e+00], [0.00000000e+00], [0.00000000e+00]])), axis=1)
+
             keypoint_coords *= output_scale
+            position.extend(spine_position)
+
 
             # TODO this isn't particularly fast, use GL for drawing and display someday...
             overlay_image = posenet.draw_skel_and_kp(
                 display_image, pose_scores, keypoint_scores, keypoint_coords,
                 min_pose_score=0.15, min_part_score=0.1)
-
+            
+            overlay_image = cv2.resize(overlay_image, dsize = (640,360), interpolation=cv2.INTER_AREA)
             cv2.imshow('posenet', overlay_image)
             frame_count += 1
             if cv2.waitKey(1) & 0xFF == ord('q'):
