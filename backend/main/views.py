25cd562f7a9972208a1f6e02fdc7ef1920fc851f
MinSeong Kang <71378475+minnseong@users.noreply.github.com>
2021-08-25 21:03:26 +0900

    Update views.py

--- backend/main/views.py
+++ backend/main/views.py
@@ -7,11 +7,5 @@ import sys
 import os
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
-# Create your views here.
-# class index(request):
-#    main.main()
-
-
 def index(request):
-    str = posenet_main.main()
-    return HttpResponse(str)
+    posenet_main.main()
